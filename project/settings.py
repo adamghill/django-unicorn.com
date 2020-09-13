cb799328a09dd43b5d35eadf79f6cef28a10c464
Adam Hill <ahill@fool.com>
2020-09-12 22:18:15 -0400

    Add sentry logging.

--- project/settings.py
+++ project/settings.py
@@ -98,3 +98,13 @@ if ENVIRONMENT == "live":
             "OPTIONS": {"CLIENT_CLASS": "django_redis.client.DefaultClient"},
         }
     }
+
+    import sentry_sdk
+    from sentry_sdk.integrations.django import DjangoIntegration
+
+    sentry_sdk.init(
+        dsn=os.environ.get("SENTRY_DSN"),
+        integrations=[DjangoIntegration()],
+        traces_sample_rate=1.0,
+        send_default_pii=True,
+    )
