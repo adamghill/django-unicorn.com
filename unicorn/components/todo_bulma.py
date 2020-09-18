7c2e2b7a824af9670000420fa70f1bdeedc80c8d
Adam Hill <ahill@fool.com>
2020-09-17 20:57:07 -0400

    Update homepage todo component.

--- unicorn/components/todo_bulma.py
+++ unicorn/components/todo_bulma.py
@@ -1,11 +1,18 @@
 from django_unicorn.components import UnicornView
+from django import forms
+
+
+class TodoForm(forms.Form):
+    task = forms.CharField(min_length=1, max_length=255, required=True)
 
 
 class TodoBulmaView(UnicornView):
+    form_class = TodoForm
+
     task = ""
     tasks = []
 
     def add(self):
-        if self.task:
+        if self.is_valid():
             self.tasks.append(self.task)
             self.task = ""
