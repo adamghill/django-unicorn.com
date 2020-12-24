d65c114adf1a77f55553be05fff4b5fd4d76b5df
Adam Hill <adamghill@gmail.com>
2020-12-24 18:00:52 -0500

    Remove aggressive caching to hopefully make sure that CSRF token is available. https://github.com/adamghill/django-unicorn/issues/102

--- www/views.py
+++ www/views.py
@@ -3,31 +3,31 @@ from django.shortcuts import render
 from django.views.decorators.cache import cache_page
 
 
-@cache_page(60 * 15)
+# @cache_page(60 * 15)
 @render_to("www/index.html")
 def index(request):
     return {}
 
 
-@cache_page(60 * 15)
+# @cache_page(60 * 15)
 @render_to("www/articles.html")
 def articles(request):
     return {}
 
 
-@cache_page(60 * 15)
+# @cache_page(60 * 15)
 def screencasts(request, name="installation"):
     template_name = f"www/screencasts/{name}.html"
     return render(request, template_name)
 
 
-@cache_page(60 * 15)
+# @cache_page(60 * 15)
 @render_to("www/sponsors.html")
 def sponsors(request):
     return {}
 
 
-@cache_page(60 * 15)
+# @cache_page(60 * 15)
 def documentation(request, name="introduction"):
     template_name = f"www/documentation/{name}.html"
     return render(request, template_name)
