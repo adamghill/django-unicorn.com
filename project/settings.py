4cb66511f23cd97e71c85a5a319f27c81ac2bc84
adamghill <adamghill@yahoo.com>
2022-01-29 15:59:19 -0500

    Prevent django-unicorn.com from using up all the Sentry transactions.

--- project/settings.py
+++ project/settings.py
@@ -126,12 +126,13 @@ if ENVIRONMENT == "live":
         }
     }
 
-    import sentry_sdk
-    from sentry_sdk.integrations.django import DjangoIntegration
+    # Prevent django-unicorn.com from using up all the Sentry transactions
+    # import sentry_sdk
+    # from sentry_sdk.integrations.django import DjangoIntegration
 
-    sentry_sdk.init(
-        dsn=os.environ.get("SENTRY_DSN"),
-        integrations=[DjangoIntegration()],
-        traces_sample_rate=1.0,
-        send_default_pii=True,
-    )
+    # sentry_sdk.init(
+    #     dsn=os.environ.get("SENTRY_DSN"),
+    #     integrations=[DjangoIntegration()],
+    #     traces_sample_rate=1.0,
+    #     send_default_pii=True,
+    # )
